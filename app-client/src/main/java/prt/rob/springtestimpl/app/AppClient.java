0e5ac45c55eac2fc12d3afe8937db96faafd3fe1
Rob Benton <rmbenton@arkbluecross.com>
2017-11-10 09:02:53 -0600

    Added the application client module.

---
+++ app-client/src/main/java/prt/rob/springtestimpl/app/AppClient.java
@@ -0,0 +1,17 @@
+package prt.rob.springtestimpl.app;
+
+/**
+ * AppClient is the application client for the spring-impl-test project. It
+ * executes business logic using the <em>provider-api</em> module without
+ * directly including any dependencies on an implementation at compile time. At
+ * runtime an implementation must be provided.
+ *
+ * @author Rob Benton
+ */
+public class AppClient
+{
+  public static void main(String[] args)
+  {
+
+  }
+}
