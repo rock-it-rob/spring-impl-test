7c1b182f1932171f7ff7b6b562574a9935ef4908
Rob Benton <rmbenton@arkbluecross.com>
2017-11-10 08:48:34 -0600

    Added the provider api.

---
+++ provider-api/src/main/java/prt/rob/springtestimpl/provider/StringValueProvider.java
@@ -0,0 +1,19 @@
+package prt.rob.springtestimpl.provider;
+
+/**
+ * StringValueProvider defines the business interface for obtaining a sample
+ * string value from an implementation.
+ *
+ * @author Rob Benton
+ */
+public interface StringValueProvider
+{
+  /**
+   * Gets a test value as a String.
+   * <p>
+   * See the respective implementation for more details on how this value is determined.
+   *
+   * @return String
+   */
+  String getValue();
+}
