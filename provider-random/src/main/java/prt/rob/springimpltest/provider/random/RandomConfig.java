8f7c8fc60db6efb5f5904ae02adf2a0767d64f3a
Rob Benton <rmbenton@arkbluecross.com>
2018-01-12 10:05:15 -0600

    Converted to annotation-driven config.

--- provider-random/src/main/java/prt/rob/springimpltest/provider/random/RandomConfig.java
+++ provider-random/src/main/java/prt/rob/springimpltest/provider/random/RandomConfig.java
@@ -2,6 +2,8 @@ package prt.rob.springimpltest.provider.random;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import prt.rob.springtestimpl.provider.StringValueProvider;
 
 import java.util.concurrent.ThreadLocalRandom;
@@ -11,18 +13,20 @@ import java.util.concurrent.ThreadLocalRandom;
  *
  * @author Rob Benton
  */
+@Configuration
 public class RandomConfig
 {
-  private static final Logger log = LoggerFactory.getLogger(RandomConfig.class.getName());
+    private static final Logger log = LoggerFactory.getLogger(RandomConfig.class.getName());
 
-  /**
-   * Gets a {@link StringValueProvider} that returns a random value.
-   *
-   * @return StringValueProvider
-   */
-  public StringValueProvider getRandomProvider()
-  {
-    log.debug("Instantiating Random long implementataion.");
-    return () -> String.valueOf(ThreadLocalRandom.current().nextLong());
-  }
+    /**
+     * Gets a {@link StringValueProvider} that returns a random value.
+     *
+     * @return StringValueProvider
+     */
+    @Bean
+    public StringValueProvider getRandomProvider()
+    {
+        log.debug("Instantiating Random long implementataion.");
+        return () -> String.valueOf(ThreadLocalRandom.current().nextLong());
+    }
 }
