cac08601912700bb1e63b1ce0474493ae3dd490d
Rob Benton <rmbenton@arkbluecross.com>
2018-01-12 10:11:29 -0600

    Converted to annotation-driven.

--- provider-timestamp/src/main/java/prt/rob/springimpltest/provider/timestamp/TimestampConfig.java
+++ provider-timestamp/src/main/java/prt/rob/springimpltest/provider/timestamp/TimestampConfig.java
@@ -2,6 +2,8 @@ package prt.rob.springimpltest.provider.timestamp;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import prt.rob.springtestimpl.provider.StringValueProvider;
 
 import java.text.SimpleDateFormat;
@@ -12,20 +14,22 @@ import java.util.Date;
  *
  * @author Rob Benton
  */
+@Configuration
 public class TimestampConfig
 {
-  private static final Logger log = LoggerFactory.getLogger(TimestampConfig.class.getName());
+    private static final Logger log = LoggerFactory.getLogger(TimestampConfig.class.getName());
 
-  /**
-   * Returns an object which implements the {@link StringValueProvider} by
-   * returning the current timestamp as a string.
-   *
-   * @return StringValueProvider
-   */
-  public StringValueProvider timestampProvider()
-  {
-    log.debug("Instantiating Timestamp provider.");
-    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh24:mm:ss.SSS");
-    return () -> sdf.format(new Date());
-  }
+    /**
+     * Returns an object which implements the {@link StringValueProvider} by
+     * returning the current timestamp as a string.
+     *
+     * @return StringValueProvider
+     */
+    @Bean
+    public StringValueProvider timestampProvider()
+    {
+        log.debug("Instantiating Timestamp provider.");
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh24:mm:ss.SSS");
+        return () -> sdf.format(new Date());
+    }
 }
--- provider-timestamp/src/test/java/prt/rob/springimpltest/provider/timestamp/TimestampTest.java
+++ provider-timestamp/src/test/java/prt/rob/springimpltest/provider/timestamp/TimestampTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 import prt.rob.springtestimpl.provider.StringValueProvider;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -17,32 +18,32 @@ import static org.junit.jupiter.api.Assertions.*;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class TimestampTest
 {
-  private static final String SPRING_CONTEXT_XML = "classpath:META-INF/provider-timestamp-spring.xml";
+    private static final String SPRING_CONTEXT_XML = "provider-timestamp-spring.xml";
 
-  private ClassPathXmlApplicationContext context;
+    private GenericXmlApplicationContext context;
 
-  @BeforeAll
-  public void beforeAll()
-  {
-    this.context = new ClassPathXmlApplicationContext(SPRING_CONTEXT_XML);
-  }
+    @BeforeAll
+    public void beforeAll()
+    {
+        this.context = new GenericXmlApplicationContext(SPRING_CONTEXT_XML);
+    }
 
-  @AfterAll
-  public void afterAll()
-  {
-    this.context.close();
-  }
+    @AfterAll
+    public void afterAll()
+    {
+        this.context.close();
+    }
 
-  /**
-   * Test the implementation.
-   */
-  @Test
-  public void testValue()
-  {
-    StringValueProvider provider = this.context.getBean(StringValueProvider.class);
-    assertNotNull(provider);
-    String value = provider.getValue();
-    assertNotNull(value);
-    assertTrue(value.length() != 0);
-  }
+    /**
+     * Test the implementation.
+     */
+    @Test
+    public void testValue()
+    {
+        StringValueProvider provider = this.context.getBean(StringValueProvider.class);
+        assertNotNull(provider);
+        String value = provider.getValue();
+        assertNotNull(value);
+        assertTrue(value.length() != 0);
+    }
 }
